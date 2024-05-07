f137cd14dd0013fbd23eb3c3acd232b3412238b5
Cameron Hardy <camhardy99@gmail.com>
2024-05-07 11:31:22 -0400

    Initial commit

---
+++ deleteUsers.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import users from './users.js';
+
+
+const orgId = 'YOUR_ORG_ID'; 
+const userIds = users; 
+const apiKey = process.env.APPLITOOLS_API_KEY;
+const rootUrl = 'https://eyes.applitools.com';  // Update rootUrl if private cloud
+
+
+// Run `node deleteUsers.js` when all variables are set and you are ready to run the script
+
+async function deleteUser(orgId, userIds, apiKey) {
+    
+    const failures = [];
+
+    for (const userId of userIds) {
+        const url = `${rootUrl}/api/Admin/orgs/${orgId}/users/${userId}?apiKey=${apiKey}`;
+        
+        try {
+            const response = await axios.delete(url);
+            
+            if (response.status !== 200) {
+                console.log(userId, 'returned status code: ', response.status);
+                failures.push(userId);
+            }
+
+            console.log(userId, "successfully removed.");
+
+        } catch (error) {
+            console.log(userId, "failed due to error:", error.response.status, error.response.statusText);
+            failures.push(userId);
+        }
+
+    }
+
+    if (failures.length > 0) {
+        console.log(`The following users failed to delete: ${failures.join(', ')}`);
+    } else {
+        console.log('All users deleted successfully.');
+    }
+}
+
+
+deleteUser(orgId, userIds, apiKey);
---
+++ users.js
@@ -0,0 +1,11 @@
+
+// Populate users array with user IDs that need to be removed from an Org.
+
+const users = [
+    'test@test.com',
+    'example@example.com',
+    'sample@sample.com'
+];
+
+
+export default users;
